33bc3f08e8008f363ef2b21932941160797b22db
jilstingray <ericl021@outlook.com>
2023-08-25 14:57:18 +0800

    Detach CommandLineRunner with Application class

---
+++ kafka-consumer-template/src/main/java/org/jilstingray/kafka/runner/CommandLineRunnerImpl.java
@@ -0,0 +1,33 @@
+package org.jilstingray.kafka.runner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jilstingray.kafka.config.KafkaConfig;
+import org.jilstingray.kafka.kafka.MultiThreadConsumer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CommandLineRunnerImpl implements CommandLineRunner {
+    @Autowired
+    private KafkaConfig kafkaConfig;
+
+    @Override
+    public void run(String... args) {
+        log.info("Starting consumer...");
+        MultiThreadConsumer consumer = new MultiThreadConsumer(
+                kafkaConfig.getBootstrapServers(),
+                kafkaConfig.getTopic(),
+                kafkaConfig.getGroupId(),
+                kafkaConfig.getConsumerId(),
+                kafkaConfig.getMaxPollRecords());
+        try {
+            while (true) {
+                consumer.run();
+            }
+        } finally {
+            consumer.close();
+        }
+    }
+}
